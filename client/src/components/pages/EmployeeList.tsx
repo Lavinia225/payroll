159e3a38fc816479969d8820f09318234616de46
Luna Seifert <thepuddingdeity@gmail.com>
2024-02-28 17:12:43 -0600

    Added functional, stylized searchbar.

--- client/src/components/pages/EmployeeList.tsx
+++ client/src/components/pages/EmployeeList.tsx
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react'
 import { useNavigate } from 'react-router-dom';
 
 interface Employee {
@@ -28,26 +28,48 @@ function EmployeeList(){
         employees: null,
         loading: true
     })
+    const [formData, setFormData] = useState("")
     const navigate = useNavigate()
 
     useEffect(()=>{
         fetch("http://127.0.0.1:5000").then(r => r.json()).then(employees => setEmployees({employees, loading: false})).catch(e=>console.warn(e))
     }, [])
 
+   /* function handleSubmit(e: React.ChangeEvent<HTMLFormElement>){
+        e.preventDefault()
+        console.log(e)
+    }*/
+
+    function findEmployees(employee: Employee){
+        const searchString = formData.toLowerCase()
+
+        return employee.first_name.toLowerCase().includes(searchString) || 
+        employee.middle_name?.toLowerCase().includes(searchString) || 
+        employee.last_name.toLowerCase().includes(searchString)
+    }
+
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>){
+        setFormData(e.target.value)
+    }
 
     if (employees.loading){
         return <p>Loading</p>
     }
-    console.log(employees)
     
     return (
         <>
             <h4>Click an employee to see their profile.</h4>
+            <form className='employee-list-searchbar'>
+                <label>Search for employee.</label>
+                <input type='text' onChange={handleChange} value={formData} />
+            </form>
             <ul className='employee-list'>
-                {employees.employees?.map(emp => <li onClick={()=>navigate(`/employees/${emp.id}`)}>{emp.last_name}, {emp.first_name}</li>)}
+                {employees.employees?.filter(findEmployees).map(emp => <li key={emp.id} onClick={()=>navigate(`/employees/${emp.id}`)}>{emp.last_name}, {emp.first_name}</li>)}
             </ul>
         </>
     )
 }
 
 export default EmployeeList
+
+//Filter based on if searched letters are present in any part of the name
